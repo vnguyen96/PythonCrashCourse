ee25fd3f589e612b6299f3f1ec0f47a18dcba5de
Vinh <vinhnguyen0096@gmail.com>
2021-05-12 18:26:25 -0700

    Adding chapter2

---
+++ Chapter2.py
@@ -0,0 +1,14 @@
+first_name = "vinh"
+last_name = "nguyen"
+full_name = f"{first_name} {last_name}"
+message = f"Hello, {full_name.title()}!"
+print(message)
+
+first = "bruce"
+last = "lee"
+full = f"{first} {last}"
+quote = "Be formless shapeless like water"
+print(f"{full.title()} once said {quote.lower()}")
+
+fav_number = 7 
+print(f"My favorite number is {fav_number}")
