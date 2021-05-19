ba8d2f9f176ee207bc51f3b219b3cb62dcf8e870
Vinh <vinhnguyen0096@gmail.com>
2021-05-18 18:48:57 -0700

    Update

---
+++ exercise1.py
@@ -0,0 +1,17 @@
+#5-1 Conditional tests 
+
+car = 'subaru'
+
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+
+dog = 'poodle'
+print("\nIs dog == 'poodle'? I predict True.")
+print(dog == 'poodle')
+
+print("\nIs dog == 'poodle'? I predict False.")
+print(dog == 'husky')
