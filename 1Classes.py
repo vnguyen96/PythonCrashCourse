9862f098b679779d9f6f89d7a49faa34a6f3ceea
Vinh Nguyen <VNguyen@skylinenet.net>
2021-07-17 17:36:55 -0400

    Update

--- 1Classes.py
+++ 1Classes.py
@@ -7,7 +7,7 @@
 class Dog:
     """A simple attempt to model a dog."""
     def __init__(self, name, age): #Defining __init__ method. A function in a class is a method. __x__ is Python's default method
-        self.name = name
+        self.name = name #variables that accessible through instances like this are called attributes 
         self.age = age
 
     def sit(self):
@@ -17,3 +17,30 @@ class Dog:
     def roll_over(self):
         """Simulate a dog rolling over in response to a command"""
         print(f"{self.name} rolled over!")
+
+
+#Making an Instance from a Class 
+
+    #Think of a class as a set of instructions for how to make an instance 
+
+my_dog = Dog('Willie', 6) #my_dog is a single instance create from class 
+
+print(f"My dog's name is {my_dog.name}.") #Here Python looks at the instance my_dog and then finds the attribute name associated with my_dog. This is the same attribute referred to as self.name in class Dog
+print(f"My dog is {my_dog.age} years old")
+
+my_dog.sit()
+my_dog.roll_over()
+
+
+#Creating multiple instances 
+
+my_dog = Dog('Willie', 6) 
+your_dog = Dog('Lucy', 3)
+
+print(f"\nMy dog's name is {my_dog.name}.")
+print(f"My dog is {my_dog.age} years old.")
+my_dog.sit()
+
+print(f"\nYour dog's name is {your_dog.name}.")
+print(f"You dog is {your_dog.age}.")
+your_dog.sit()
---
+++ exercise1.py
@@ -0,0 +1,57 @@
+#9-1 Restaurant
+
+class Restaurant:
+    def __init__(self, restaurant_name, crusine_type):
+        self.restaurant_name = restaurant_name
+        self.crusine_type = crusine_type
+
+    def describe_restaurant(self):
+        print(f"Restaurant name is {self.restaurant_name}")
+        print(f"Restaurant crusine is {self.crusine_type}")
+
+    def open_restaurant(self):
+        print(f"{self.restaurant_name} is open!")
+
+
+restaurant = Restaurant('PhoKing', 'Vietnamese')
+
+restaurant.describe_restaurant()
+restaurant.open_restaurant()
+
+restaurant_two = Restaurant('La Toteca', 'Mexican')
+
+restaurant_two.describe_restaurant()
+
+restaurant_three = Restaurant('Mama Roma', 'Italian')
+
+restaurant_three.describe_restaurant()
+
+
+#9-3
+
+class User:
+    def __init__(self, first_name, last_name, age, position, location):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.position = position
+        self.location = location
+
+    def describe_user(self):
+        print(f"\nUser first name is {self.first_name.title()}.")
+        print(f"User last name is {self.last_name.title()}.")
+        print(f"User is {self.age} years old.")
+        print(f"User is a {self.position}.")
+        print(f"User is located at {self.location} facility.")
+
+    def greeting_user(self):
+        print(f"Hello {self.first_name} {self.last_name}!")
+
+
+user_one = User('vinh', 'nguyen', '24', 'NOC Tech', 'GB')
+user_one.describe_user()
+user_one.greeting_user()
+
+user_one = User('dan', 'schupple', '31', 'NOC Manager', 'GB')
+user_one.describe_user()
+user_one.greeting_user()
